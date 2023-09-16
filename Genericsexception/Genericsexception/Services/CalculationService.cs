20b64233d826c45fe6592e29fcc55de711ea4813
Thomas Caires <thomas_cairesaraujo@outlook.com>
2023-09-16 11:57:49 -0300

    Generics Exceptions

---
+++ Genericsexception/Genericsexception/Services/CalculationService.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Genericsexception.Services
+{
+    internal class CalculationService
+    {
+        public T Max<T>(List<T> list) where T : IComparable
+        {
+            if (list.Count == 0)
+            {
+                throw new Exception("The list can not be empty");
+            }
+
+            T max = list[0];
+            for (int i = 0; i < list.Count; i++)
+            {
+                max = list[i];
+            }
+            return max;
+        }
+    }
+}
